dceff51ecd0f97cb32696c003b8f29a86495295c
trymiz <trymmagnusdal@gmail.com>
2024-01-26 14:29:48 +0100

    First commit

---
+++ modules/user.mjs
@@ -0,0 +1,11 @@
+class User {
+
+    constructor() {
+        ///TODO: Are these the correct fields for your project?
+        this.email;
+        this.pswHash;
+        this.name;
+    }
+}
+
+export default User;
---
+++ server.mjs
@@ -0,0 +1,24 @@
+import express from 'express' // Express is installed using npm
+import USER_API from './routes/usersRoute.mjs'; // This is where we have defined the API for working with users.
+
+// Creating an instance of the server
+const server = express();
+// Selecting a port for the server to use.
+const port = (process.env.PORT || 8080);
+server.set('port', port);
+
+// Defining a folder that will contain static files.
+server.use(express.static('public'));
+
+// Telling the server to use the USER_API (all urls that uses this code will have to have the /user after the base address)
+server.use("/user", USER_API);
+
+// A get request handler example)
+server.get("/", (req, res, next) => {
+    res.status(200).send(JSON.stringify({ msg: "These are not the droids...." })).end();
+});
+
+// Start the server 
+server.listen(server.get('port'), function () {
+    console.log('server running', server.get('port'));
+});
